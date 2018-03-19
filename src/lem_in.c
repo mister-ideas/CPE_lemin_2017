53cd27eb31f04b95c7f761cb58f614472b55be46
Vincent Attali <vincent.attali@epitech.eu>
2018-03-19 17:15:44 +0100

    "started parsing"

--- src/lem_in.c
+++ src/lem_in.c
@@ -5,8 +5,16 @@
 ** lem_in.c
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int parsing(void)
 {
+	struct anthil anthil = malloc(sizeof(anthil));
+	char *cur_data = NULL;
+
+	if (!anthil)
+		return (84);
 	return (0);
 }
 
