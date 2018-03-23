b4c14ba6d5bc71744857f794cb7ce3ceb446366f
Vincent Attali <vincent.attali@epitech.eu>
2018-03-23 14:45:58 +0100

    "pushed parsing branch"

---
+++ include/lem_in.h
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2018
+** Lem-in
+** File description:
+** lem_in.h
+*/
+
+#include "linked_list.h"
+
+#ifndef LEM_IN_H_
+#define LEM_IN_H_
+
+typedef struct anthil {
+	int nb_ants;
+	int nb_rooms;
+	char *start;
+	char *end;
+	rooms_t *rooms;
+	tunnels_t *tunnels;
+	int is_start;
+	int is_end;
+} anthil_t;
+
+/* parsing.c */
+
+int parsing_init(anthil_t *anthil);
+
+/* get_names.c */
+
+int get_start_end_names(anthil_t *anthil, char *cur_data, char *name1,
+			char *name2);
+int get_room_name(anthil_t *anthil, char *cur_data, char *name1, char *name2);
+int get_tunnel_names(anthil_t *anthil, char *cur_data,
+		char *name1, char *name2);
+char *get_names(anthil_t *anthil, char *cur_data, int mode);
+
+/* detect.c */
+
+int detect_commands(anthil_t *anthil, char *cur_data);
+
+/* display.c */
+
+void initial_display(anthil_t *anthil);
+
+#endif
---
+++ src/lem_in.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2018
+** Lem-in
+** File description:
+** lem_in.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "lem_in.h"
+
+int main(int ac, char **av)
+{
+	anthil_t *anthil;
+
+	if (ac > 1)
+		return (84);
+	anthil = malloc(sizeof(*anthil));
+	if (anthil == NULL)
+		return (84);
+	if (parsing_init(anthil) == 84)
+		return (84);
+	return (0);
+}
